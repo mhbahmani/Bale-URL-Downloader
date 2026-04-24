196ef9006f9f7a4d987b3d12f2d4c2f3cee8c2fa
MohammadHosein Bahmani <mhossein.bahmani@divar.ir>
2026-04-24 23:34:10 +0330

    Add TWITTER_OUTPUT_DIR env var

--- bale_downloader/config.py
+++ bale_downloader/config.py
@@ -9,6 +9,7 @@ GOOGLE_DRIVE_FOLDER_ID = os.environ["GOOGLE_DRIVE_FOLDER_ID"]
 ALLOWED_CHAT_IDS = [int(x) for x in os.environ["ALLOWED_CHAT_IDS"].split(",")]
 COOKIE_PATH = os.environ.get("COOKIE_PATH", "firefox-cookies")
 OUTPUT_DIR = os.environ.get("OUTPUT_DIR", "yt-output")
+TWITTER_OUTPUT_DIR = os.environ.get("TWITTER_OUTPUT_DIR", "twitter-output")
 TARGET_SIZE_MB = int(os.environ.get("TARGET_SIZE_MB", "20"))
 GOOD_QUALITY_SIZE_LIMIT_MB = int(os.environ.get("GOOD_QUALITY_SIZE_LIMIT_MB", "1000"))
 SPLIT_VIDEO_IF_ITS_TOO_LARGE = bool(os.environ.get("SPLIT_VIDEO_IF_ITS_TOO_LARGE", False))
