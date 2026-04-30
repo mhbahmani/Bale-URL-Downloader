a016051a71c12e63a9764769f6d42ea1463eae46
MohammadHosein Bahmani <mhossein.bahmani@divar.ir>
2026-05-01 00:27:15 +0330

    Implement github single file download

--- bale_downloader/github.py
+++ bale_downloader/github.py
@@ -1,10 +1,12 @@
+from bale_downloader.utils import download_file
+
 from enum import Enum
 
 import re
 
 
 class Github:
-    GITHUB_URL_REGEX = "http(s)?://github.com/(?P<group_name>\\w+)/(?P<project_name>\\w+)(/(?P<repo_location>[\\w/-]+))?"
+    GITHUB_URL_REGEX = "http(s)?://github.com/(?P<group_name>[\\w_-]+)/(?P<project_name>[\\w_-]+)(/(?P<repo_location>((blob|tree)/(?P<branch>[\\w.]+)/(?P<file_path>[\\w./_-]+)|[\\w./_-]+)))?"
     class GithubURLType(Enum):
         REPO = "repo"
         WEBPAGE = "webpage"
@@ -12,17 +14,23 @@ class Github:
 
     def __init__(self, url) -> None:
         self.url = url
-        self.group_name, self.project_name, self.repo_location = self._parse_url()
+        self._sanitize_url()
+        self.group_name, self.project_name, self.repo_location, self.branch, self.file_path = self._parse_url()
         self.url_type = self._get_url_type()
 
-    def _parse_url(self) -> GithubURLType:
+    def _parse_url(self) -> tuple[str, str, str, str | None, str | None]:
         pattern_matching_result = re.search(Github.GITHUB_URL_REGEX, self.url)
         if not pattern_matching_result:
             return None, None, None
         return \
-            pattern_matching_result.groupdict["group_name"], \
-            pattern_matching_result.groupdict["project_name"], \
-            pattern_matching_result.groupdict["repo_location"]
+            pattern_matching_result.groupdict()["group_name"], \
+            pattern_matching_result.groupdict()["project_name"], \
+            pattern_matching_result.groupdict()["repo_location"], \
+            pattern_matching_result.groupdict().get("branch"), \
+            pattern_matching_result.groupdict().get("file_path")
+
+    def _sanitize_url(self):
+        self.url = self.url.replace("/refs/heads", "")
 
     def _get_url_type(self) -> GithubURLType:
         if self.repo_location:
@@ -38,11 +46,14 @@ class Github:
 
     def get_content(self) -> tuple[str, str, list[str], list[str]]:
         if self.url_type == Github.GithubURLType.REPO:
+            print("Clone the whole repo")
             return self._clone_repo()
         if self.url_type == Github.GithubURLType.WEBPAGE:
+            print("Export like a webpage")
             return self._export_webpage()
         if self.url_type == Github.GithubURLType.SINGLE_FILE:
-            return self._donwload_single_file()
+            print("Download single file")
+            return self.file_path, None, [self._donwload_single_file()], []
 
     def _clone_repo(self):
         pass
@@ -51,4 +62,15 @@ class Github:
         pass
 
     def _donwload_single_file(self):
-        pass
+        file_path = self._get_file_name_to_save()
+        download_file(
+            self._generate_single_file_download_url(),
+            file_path
+        )
+        return file_path
+
+    def _get_file_name_to_save(self) -> str:
+        return self.file_path.replace("/", "-")
+
+    def _generate_single_file_download_url(self):
+        return f"https://github.com/{self.group_name}/{self.project_name}/raw/refs/heads/{self.branch}/{self.file_path}"
